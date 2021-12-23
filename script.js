b822e32584609d71430ea8163f940ecff0d82225
empipio <94937507+empipio@users.noreply.github.com>
2021-12-23 21:49:31 +0000

    password length

--- script.js
+++ script.js
@@ -26,11 +26,11 @@ do i want numbers true/false
 
 //random choice
 
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 
-var numbers = ["1234567890"]
+var numbers = "1234567890";
 
 var symbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
@@ -41,7 +41,26 @@ var symbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() { 
+
+  var passwordLength = window.prompt("Select number of characters, 8-128");
+
+  if (passwordLength <8) {
+    return;
+  }
+
+  if (passwordLength >128) {
+    return;
+  }
+
+  var selectUpper = window.confirm("Would you like to include uppercase letters?");
+
+  var selectLower = window.confirm("Would you like to include lowercase letters?");
+
+  var selectNumbers = window.confirm("Would you like to include numbers?");
+
+  var selectSymbols = window.confirm("Would you like to include special characters?");
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
