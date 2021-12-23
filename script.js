0934a08eccf98dbe4cbce29cbcfe3605f27941a4
empipio <94937507+empipio@users.noreply.github.com>
2021-12-23 21:01:55 +0000

    pseudocode added

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+//generate password
+
+//symbols
+
+//letters uppercase
+
+//letters lowercase
+
+//numbers
+
+//choose password length 8-128 characters
+
+/*
+
+do i want symbols true/false
+
+do i want lowercase true/false
+
+do i want uppercase true/false
+
+do i want numbers true/false
+
+*/
+
+//assemble password
+
+//random choice
+
+var symbols = 
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
