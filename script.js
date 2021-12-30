1af233869e6d758ef2b4673d4acbc8ec5d611cae
empipio <94937507+empipio@users.noreply.github.com>
2021-12-30 16:24:21 +0000

    now generates password

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //generate password
 
-//symbols
+//symbols /
 
-//letters uppercase
+//letters uppercase /
 
-//letters lowercase
+//letters lowercase /
 
-//numbers
+//numbers /
 
-//choose password length 8-128 characters
+//choose password length 8-128 characters /
 
 /*
 
@@ -22,75 +22,179 @@ do i want numbers true/false
 
 */
 
-//assemble password
+//assemble password /
 
-//random choice
+//random choice /
 
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// prompt if nothing selected
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-
-var numbers = "1234567890";
-
-var symbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-
-charactersUsed = []
-
-
+// some way of ensuring one of each criteria has been added to the password
 
 //Assignment Code
 
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+var symbols = [
+  "/",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "+",
+  "\\",
+  "-",
+  "=",
+  "[",
+  "]",
+  "{",
+  "}",
+  ";",
+  "'",
+  ":",
+  '"',
+  "|",
+  ",",
+  ".",
+  "<",
+  ">",
+  "?",
+  "+",
+];
+
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() { 
-
+function generatePassword() {
   var passwordLength = window.prompt("Select number of characters, 8-128");
 
-  if (passwordLength <8) {
+  if (passwordLength < 8) {
     return;
   }
 
-  if (passwordLength >128) {
+  if (passwordLength > 128) {
     return;
   }
 
-  var selectUpper = window.confirm("Would you like to include uppercase letters?");
+  var selectUpper = window.confirm(
+    "Would you like to include uppercase letters?"
+  );
 
-  var selectLower = window.confirm("Would you like to include lowercase letters?");
+  var selectLower = window.confirm(
+    "Would you like to include lowercase letters?"
+  );
 
   var selectNumbers = window.confirm("Would you like to include numbers?");
 
-  var selectSymbols = window.confirm("Would you like to include special characters?");
+  var selectSymbols = window.confirm(
+    "Would you like to include special characters?"
+  );
 
+  var charactersUsed = [];
 
+  if (selectUpper) {
+    charactersUsed = charactersUsed.concat(upperCase);
+  }
+
+  if (selectLower) {
+    charactersUsed = charactersUsed.concat(lowerCase);
+  }
+
+  if (selectNumbers) {
+    charactersUsed = charactersUsed.concat(numbers);
+  }
+
+  if (selectSymbols) {
+    charactersUsed = charactersUsed.concat(symbols);
+  }
+  
+  var password = ""
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charactersUsed.length);
+    var randomCharacter = charactersUsed[randomIndex];
+    password = password + randomCharacter
+  }
+
+  return password;
+}
+
+// Write password to the #password input
+function writePassword() {
   var password = generatePassword();
-  if (selectUpper = true) {
-    charactersUsed.concat(selectUpper)
-  } 
-  if (selectLower = true) {
-    charactersUsed.concat(selectLower)
-  }
-  if (selectNumbers = true) {
-    charactersUsed.concat(selectNumbers)
-  }
-  if (selectSymbols = true) {
-    charactersUsed.concat(selectSymbols)
-  }
-  //for loop here?
-  for (i = 0; i < passwordLength; i++) {
-    //random character/number generation
-  } 
 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
   //display password in window.alert
-
+  window.alert("here is your password: " + password);
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-writePassword();
